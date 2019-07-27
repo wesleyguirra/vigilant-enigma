38c1e8fd9f9032f0040bd0dede53d3133bc52db7
Wes Guirra <wesleeycosta7@gmail.com>
2019-07-27 01:11:45 -0300

    Fixed recharge amout type

--- app/src/main/java/br/com/m2yconductorservices/utils/extensions/M2YCDTStringExtensions.kt
+++ app/src/main/java/br/com/m2yconductorservices/utils/extensions/M2YCDTStringExtensions.kt
@@ -18,9 +18,23 @@ fun Float.m2yCdtFormatToCurrency(locale: Locale): String {
 }
 
 /**
+ * @param locale locale of the currency to m2yCdtFormat
+ * @return the value with the locale currency
+ */
+fun Int.m2yCdtFormatToCurrency(locale: Locale): String {
+    val currencyFormatter = NumberFormat.getCurrencyInstance(locale)
+    return currencyFormatter.format(this.toDouble())
+}
+
+/**
  * @return the value with the BRL currency
  */
 fun Float.m2yCdtFormatCurrencyBRL(trimRs: Boolean = false): String {
+    val locale = Locale(M2YCDTConstants.LANGUAGE_PT, M2YCDTConstants.COUNTRY_BR)
+    return if (trimRs) this.m2yCdtFormatToCurrency(locale).replace("R$", "") else this.m2yCdtFormatToCurrency(locale)
+}
+
+fun Int.m2yCdtFormatCurrencyBRL(trimRs: Boolean = false): String {
     val locale = Locale(M2YCDTConstants.LANGUAGE_PT, M2YCDTConstants.COUNTRY_BR)
     return if (trimRs) this.m2yCdtFormatToCurrency(locale).replace("R$", "") else this.m2yCdtFormatToCurrency(locale)
 }
